0a5e9a1502776ff486f6a3e76f3201f2f516df06
Karthik <gkarthik24@gmail.com>
2020-08-26 09:33:02 +0530

    Final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: GetMagic(),
+        ),
       ),
     );
+
+class GetMagic extends StatefulWidget {
+  @override
+  _GetMagicState createState() => _GetMagicState();
+}
+
+class _GetMagicState extends State<GetMagic> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.blue,
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),
+      ),
+    );
+  }
+}
